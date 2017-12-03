f0f44d4501142b394577d7de911b1050bc426b2d
Erik Phillips <erik.phillips@icloud.com>
2017-12-03 09:37:44 -0800

    initial commit of recomendation algorithim

---
+++ recommend.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import graphlab
+
+# pass in column names for each CSV and read them using pandas. 
+# Column names available in the readme file
+
+#Reading users file:
+u_cols = ['user_id', 'age', 'sex', 'occupation', 'zip_code']
+users = pd.read_csv('ml-100k/u.user', sep='|', names=u_cols,
+ encoding='latin-1')
+
+#Reading ratings file:
+r_cols = ['user_id', 'movie_id', 'rating', 'unix_timestamp']
+ratings = pd.read_csv('ml-100k/u.data', sep='\t', names=r_cols,
+ encoding='latin-1')
+
+#Reading items file:
+i_cols = ['movie id', 'movie title' ,'release date','video release date', 'IMDb URL', 'unknown', 'Action', 'Adventure',
+ 'Animation', 'Children\'s', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Fantasy',
+ 'Film-Noir', 'Horror', 'Musical', 'Mystery', 'Romance', 'Sci-Fi', 'Thriller', 'War', 'Western']
+items = pd.read_csv('ml-100k/u.item', sep='|', names=i_cols,
+ encoding='latin-1')
+
+print "\nUSERS:"
+print users.shape
+print users.head()
+
+print "\nRATINGS:"
+print ratings.shape
+print ratings.head()
+
+print "\nITEMS:"
+print items.shape
+print items.head()
+
+r_cols = ['user_id', 'movie_id', 'rating', 'unix_timestamp']
+ratings_base = pd.read_csv('ml-100k/ua.base', sep='\t', names=r_cols, encoding='latin-1')
+ratings_test = pd.read_csv('ml-100k/ua.test', sep='\t', names=r_cols, encoding='latin-1')
+print ratings_base.shape, ratings_test.shape
+
+train_data = graphlab.SFrame(ratings_base)
+test_data = graphlab.SFrame(ratings_test)
+
+popularity_model = graphlab.popularity_recommender.create(train_data, user_id='user_id', item_id='movie_id', target='rating')
+
+#Get recommendations for first 5 users and print them
+#users = range(1,6) specifies user ID of first 5 users
+#k=5 specifies top 5 recommendations to be given
+popularity_recomm = popularity_model.recommend(users=range(1,6),k=5)
+popularity_recomm.print_rows(num_rows=25)
+
+print ratings_base.groupby(by='movie_id')['rating'].mean().sort_values(ascending=False).head(20)
+
+#Train Model
+item_sim_model = graphlab.item_similarity_recommender.create(train_data, user_id='user_id', item_id='movie_id', target='rating', similarity_type='pearson')
+
+#Make Recommendations:
+item_sim_recomm = item_sim_model.recommend(users=range(1,6),k=5)
+item_sim_recomm.print_rows(num_rows=25)
+
+model_performance = graphlab.compare(test_data, [popularity_model, item_sim_model])
+graphlab.show_comparison(model_performance,[popularity_model, item_sim_model])
+
